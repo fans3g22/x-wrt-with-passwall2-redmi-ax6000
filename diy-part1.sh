7ce5027d4fafcb46d124f7f79919a6660640e1b4
fans3g22 <104512665+fans3g22@users.noreply.github.com>
2025-10-27 10:52:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
-#
+#=================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
-echo "src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome;main" >> "feeds.conf.default"
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#=================================================
 
-# Add a feed source
-# passwall
-#echo 'src-git-full passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
-#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
-# mosdns
-#echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns.git' >> feeds.conf.default
+set -e
 
-# Add extra package
-# dogcom
-#git clone https://github.com/mchome/openwrt-dogcom package/openwrt-dogcom
+# 备份原始 feeds.conf.default
+cp feeds.conf.default feeds.conf.default.bak
+
+# 添加额外 feed 源
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
+echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome;main' >> feeds.conf.default
+
+#（可选）查看文件结构用于调试
+echo "=== feeds.conf.default 内容 ==="
+cat feeds.conf.default
